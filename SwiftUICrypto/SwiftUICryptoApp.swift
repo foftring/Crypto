6b815c2c793196ac947d5109d9f55fffb6e9f7f7
Frank Oftring <frank.oftring@protonmail.com>
2021-12-01 21:38:31 -0500

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Frank Oftring on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
